72c5026e9d0610e4b96948d8759fe042f2c6bd70
Oleg Ch <056@live.ru>
2019-05-18 15:31:03 +0300

    Add new page contact list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { NavLink, Route } from 'react-router-dom';
+
+import ContactsListPage from './pages/contacts-list-page'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
         <div>
-          <div className="ui two item menu">
+          <div className="">
             <NavLink className="item" activeClassName="active" exact to="/">Contacts List</NavLink>
             <NavLink className="item" activeClassName="active" exact to="/contacts/new">Add Contact</NavLink>
           </div>
-          {/*<Route exact path="/" component={ContactsListPage}/>*/}
+          {<Route exact path="/" component={ContactsListPage}/>}
+          {/*<Route path="/contacts/call/:_id" component={CallHistoryPage}/>*/}
           {/*<Route path="/contacts/new" component={ContactsFormPage}/>*/}
           {/*<Route path="/contacts/edit/:_id" component={ContactsFormPage}/>*/}
         </div>
---
+++ src/pages/contacts-list-page.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class ContactsListPage extends Component {
+    render() {
+        return <h1>ContactsListPage</h1>
+    }
+}
+
+export default ContactsListPage;
