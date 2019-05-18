a73d8b4e43ef8890eb6d92ef97a210ff28316c34
Oleg Ch <056@live.ru>
2019-05-18 21:13:52 +0300

    Add Provider

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import allReducers from "./reducers";
+const store = createStore(allReducers);
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>
     , document.getElementById('root'));
 
