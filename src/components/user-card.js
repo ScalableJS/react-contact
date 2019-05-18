c74595e3ee7d3a4dfbb5accb6383e3f2c9a37c87
Oleg Ch <056@live.ru>
2019-05-19 00:09:12 +0300

    Add click event and design

--- src/components/user-card.js
+++ src/components/user-card.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import {Card} from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Card, Button} from 'react-bootstrap';
 
-export default function UserCard({contact, deleteContact}) {
-    return (
-        <Card>
-            <Card.Body>
-                <Card.Title>
-                    <div name='user'/>
-                    {contact.name.first} {contact.name.last}
-                </Card.Title>
-                <Card.Text>
-                    <p>
-                        <div name='phone'/>
-                        {contact.phone}</p>
-                    <p>
-                        <div name='mail'/>
-                        {contact.email}</p>
-                </Card.Text>
-            </Card.Body>
+class UserCard extends Component {
+    deleteCard() {
+        alert('removed')
+    }
+    callPhone() {
+        alert('call')
+    }
 
-        </Card>
-    )
+    render() {
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Title>
+                        <div name='user'/>
+                        {this.props.contact.name.first} {this.props.contact.name.last}
+                    </Card.Title>
+                    <Card.Text>
+                        <p>
+                            <div name='phone'/>
+                            <a href={`tel:${this.props.contact.phone}`}
+                               onClick={() => this.callPhone(this.props.contact._id)}
+                            >{this.props.contact.phone}</a>
+                        </p>
+                        <p>
+                            <div name='mail'/>
+                            <a href={`mailto:${this.props.contact.email}`}>{this.props.contact.email}</a>
+
+                        </p>
+
+                    </Card.Text>
+                    <hr/>
+                    <Button variant="link" href={`/contacts/edit/${this.props.contact._id}`}> Edit</Button>
+                    <Button variant="primary"
+                            onClick={() => this.deleteCard(this.props.contact._id)}> Delete</Button>
+                </Card.Body>
+            </Card>
+        )
+    }
 }
+
+export default UserCard;
