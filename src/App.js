36bdfb26867696354408b443217eeaeb3c95aeb8
Oleg Ch <056@live.ru>
2019-05-18 15:39:09 +0300

    Add new page contact form for update contacts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { NavLink, Route } from 'react-router-dom';
 
 import ContactsListPage from './pages/contacts-list-page'
+import ContactsFormPage from './pages/contacts-form-page'
+
 import './App.css';
 
 class App extends Component {
@@ -12,10 +14,12 @@ class App extends Component {
             <NavLink className="item" activeClassName="active" exact to="/">Contacts List</NavLink>
             <NavLink className="item" activeClassName="active" exact to="/contacts/new">Add Contact</NavLink>
           </div>
-          {<Route exact path="/" component={ContactsListPage}/>}
+          <Route exact path="/" component={ContactsListPage}/>
+          <Route path="/contacts/new" component={ContactsFormPage}/>
+            {/*<Route path="/contacts/edit/:_id" component={ContactsFormPage}/>*/}
           {/*<Route path="/contacts/call/:_id" component={CallHistoryPage}/>*/}
-          {/*<Route path="/contacts/new" component={ContactsFormPage}/>*/}
-          {/*<Route path="/contacts/edit/:_id" component={ContactsFormPage}/>*/}
+
+
         </div>
     );
   }
---
+++ src/pages/contacts-form-page.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class ContactsFormPage extends Component {
+    render() {
+        return <h1>ContactsFormPage</h1>
+    }
+}
+
+export default ContactsFormPage;
