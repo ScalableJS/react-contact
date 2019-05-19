921c44172c81cd2354b962743d04d942df0965fb
Oleg Ch <056@live.ru>
2019-05-19 13:23:07 +0300

    add find contact by id

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,25 @@
-import contact from '../DummyData/dummy-users'
+import contacts from '../DummyData/dummy-users'
+import actionEnum from './actionsEnum';
 
 export const getAllContacts = () => {
     return dispatch => {
         setTimeout(() => {
             dispatch({
-                type: 'GET_ALL_CONTACTS',
-                payload: contact
+                type: actionEnum.GET_ALL_CONTACTS,
+                payload: contacts
             });
         }, 1000);
     }
 };
+
+export function getContact(_id) {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch({
+                type: actionEnum.GET_CONTACT,
+                payload: contacts.find((item)=>item._id === _id)
+
+            });
+        }, 1000);
+    }
+}
