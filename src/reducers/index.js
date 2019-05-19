669c61fa57396e21d62b5e963804d2eb0370c9f7
Oleg Ch <056@live.ru>
2019-05-19 10:43:31 +0300

    replace contact to Object for more data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux';
 import ContactsReducer from './contacts-reducer';
 
 const allReducers = combineReducers({
-    contacts: ContactsReducer
+    contactStore: ContactsReducer
 });
 
 
