f3f22c62592fb9435742d6c1f742b2a53a364307
Oleg Ch <056@live.ru>
2019-05-20 00:25:50 +0300

    Add icons

--- src/components/user-card.js
+++ src/components/user-card.js
@@ -1,21 +1,34 @@
 import React, {Component} from 'react';
+import {Icon} from 'react-icons-kit'
+import {user} from 'react-icons-kit/icomoon/user'
+import {phone} from 'react-icons-kit/icomoon/phone'
+import {mail2 as mail} from 'react-icons-kit/icomoon/mail2'
+
+
 import {Card, Button} from 'react-bootstrap';
 
 export default function UserCard({contact, deleteContact, callPhone}) {
     return (
-        <Card>
+        <Card className={"m-2"}>
             <Card.Body>
                 <Card.Title>
-                    <span name='user'/>
-                    {contact.name.first} {contact.name.last}
+                    <span className={'mr-1'} style={{width: 24, height: 24, display: 'inline-block', color: '#2A9D8F'}}>
+                        <Icon size={'100%'} icon={user}/>
+                    </span>
+
+                    <span className="align-middle">{contact.name.first} {contact.name.last}</span>
                 </Card.Title>
                 <Card.Text>
-                    <span name='phone'/>
+                    <span className={'mr-1'} style={{width: 24, height: 24, display: 'inline-block', color: '#2A9D8F'}}>
+                        <Icon size={'100%'} icon={phone}/>
+                    </span>
                     <a href={`tel:${contact.phone}`}
                        onClick={() => callPhone(contact._id)}
                     >{contact.phone}</a>
                 </Card.Text>
-                <span name='mail'/>
+                <span className={'mr-1'} style={{width: 24, height: 24, display: 'inline-block', color: '#2A9D8F'}}>
+                    <Icon size={'100%'} icon={mail}/>
+                </span>
                 <a href={`mailto:${contact.email}`}>{contact.email}</a>
                 <Card.Text>
                 </Card.Text>
