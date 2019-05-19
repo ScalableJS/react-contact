055f9ebc7218c78bcbd9ccbb1d7571aa48dc753d
Oleg Ch <056@live.ru>
2019-05-19 22:10:08 +0300

    add update save

--- src/pages/contacts-form-page.js
+++ src/pages/contacts-form-page.js
@@ -1,49 +1,81 @@
 import React, {Component} from 'react';
+import {Redirect} from 'react-router';
 import {bindActionCreators} from 'redux';
 import {connect} from "react-redux";
-import {getContact} from '../actions';
+import {SubmissionError} from 'redux-form';
 import UserForm from '../components/user-form';
-
-
+import {getContact, newContact, updateContact, saveContact} from '../actions';
 
 
 class ContactsFormPage extends Component {
+    state = {
+        redirect: false
+    }
+
     componentDidMount() {
-        const { _id } = this.props.match.params;
-        if(_id) {
+        const {_id} = this.props.match.params;
+        if (_id) {
             this.props.getContact(_id);
         } else {
-
+            this.props.newContact();
         }
     }
 
     submit = (contact) => {
-        alert('submited')
+        if (contact._id) {
+            return this.props.updateContact(contact)
+                .then(response => this.setState({redirect: true}))
+                .catch(err => {throw new SubmissionError(this.props.errors)})
+        } else {
+            return this.props.saveContact(contact)
+                .then(response => this.setState({redirect: true}))
+                .catch(err => {throw new SubmissionError(this.props.errors)})
+        }
     };
 
 
     render() {
-        if (this.props.contact) {
-            return (
-                <UserForm
-                    onSubmit={this.submit} contact={this.props.contact}></UserForm>
-            )
+        const {_id} = this.props.match.params;
+        if (this.state.redirect) {
+            return <Redirect to="/"/>
         } else {
-            return <h1>Loading ...</h1>
+            if (_id) {
+                if (this.props.contact) {
+                    return (
+                        <UserForm
+                            onSubmit={this.submit}
+                            contact={this.props.contact}
+                            isNew={false}
+                        />)
+                } else {
+                    return <h1>Loading ...</h1>
+                }
+
+            } else {
+                return (
+                    <UserForm
+                        onSubmit={this.submit}
+                        contact={{}}
+                        isNew={true}
+
+                    />)
+            }
+
+
         }
     }
 }
 
 
-
 function mapStateToProps(state) {
     return {
-        contact: state.contactStore.contact
+        contact: state.contactStore.contact,
+        contacts: state.contactStore.contacts,
     }
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({getContact: getContact}, dispatch)
+    return bindActionCreators({getContact, newContact, updateContact, saveContact}, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ContactsFormPage);
