203a2c4102cc50115d834d0a0b28f450f17cadf2
Oleg Ch <056@live.ru>
2019-05-19 15:22:02 +0300

    update tag p

--- src/components/user-card.js
+++ src/components/user-card.js
@@ -5,6 +5,7 @@ class UserCard extends Component {
     deleteCard() {
         alert('removed')
     }
+
     callPhone() {
         alert('call')
     }
@@ -14,22 +15,18 @@ class UserCard extends Component {
             <Card>
                 <Card.Body>
                     <Card.Title>
-                        <div name='user'/>
+                        <span name='user'/>
                         {this.props.contact.name.first} {this.props.contact.name.last}
                     </Card.Title>
                     <Card.Text>
-                        <p>
-                            <div name='phone'/>
+                            <span name='phone'/>
                             <a href={`tel:${this.props.contact.phone}`}
                                onClick={() => this.callPhone(this.props.contact._id)}
                             >{this.props.contact.phone}</a>
-                        </p>
-                        <p>
-                            <div name='mail'/>
-                            <a href={`mailto:${this.props.contact.email}`}>{this.props.contact.email}</a>
-
-                        </p>
-
+                    </Card.Text>
+                        <span name='mail'/>
+                        <a href={`mailto:${this.props.contact.email}`}>{this.props.contact.email}</a>
+                    <Card.Text>
                     </Card.Text>
                     <hr/>
                     <Button variant="link" href={`/contacts/edit/${this.props.contact._id}`}> Edit</Button>
