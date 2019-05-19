0f5a6a6ca6eafac68cca5170cdcc29dc6442e005
Oleg Ch <056@live.ru>
2019-05-19 13:36:26 +0300

    add dummy submit

--- src/pages/contacts-form-page.js
+++ src/pages/contacts-form-page.js
@@ -1,9 +1,49 @@
 import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from "react-redux";
+import {getContact} from '../actions';
+import UserForm from '../components/user-form';
+
+
+
 
 class ContactsFormPage extends Component {
+    componentDidMount() {
+        const { _id } = this.props.match.params;
+        if(_id) {
+            this.props.getContact(_id);
+        } else {
+
+        }
+    }
+
+    submit = (contact) => {
+        alert('submited')
+    };
+
+
     render() {
-        return <h1>ContactsFormPage</h1>
+        if (this.props.contact) {
+            return (
+                <UserForm
+                    onSubmit={this.submit} contact={this.props.contact}></UserForm>
+            )
+        } else {
+            return <h1>Loading ...</h1>
+        }
     }
 }
 
-export default ContactsFormPage;
+
+
+function mapStateToProps(state) {
+    return {
+        contact: state.contactStore.contact
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({getContact: getContact}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ContactsFormPage);
