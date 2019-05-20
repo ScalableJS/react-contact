9d7ea60041be1ca94a6118ac7d9435dcd55639b4
Oleg Ch <056@live.ru>
2019-05-20 14:05:15 +0300

    Add user call history of components

---
+++ src/components/user-call.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function UserCall({contact}) {
+    return (
+        <div>Call history</div>
+    )
+}
--- src/pages/contacts-call-page.js
+++ src/pages/contacts-call-page.js
@@ -1,13 +1,54 @@
 import React, {Component} from 'react';
+import {Redirect} from 'react-router';
+import UserCall from "../components/user-call";
+
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {Row, Col} from 'react-bootstrap';
+import {getContact, newContact, saveContact, updateContact} from "../actions";
 
 
 class ContactsCallPage extends Component {
+    state = {
+        redirect: false
+    };
+
+    componentDidMount() {
+        const {_id} = this.props.match.params;
+        if (_id) {
+            this.props.getContact(_id);
+        }
+    }
+
     render() {
-        return <h1>Loading ...</h1>
+        const {_id} = this.props.match.params;
+        if (this.state.redirect) {
+            return <Redirect to="/"/>
+        } else {
+            if (_id) {
+                if (this.props.contact) {
+                    return (
+                        <UserCall
+                            contact={this.props.contact}
+                        />)
+                } else if (this.props.contact === null) {
+                    return <h1>The contact is missing or deleted</h1>
+                } else {
+                    return <h1>Loading ...</h1>
+                }
+            }
+        }
     }
 }
 
-export default ContactsCallPage;
+function mapStateToProps(state) {
+    return {
+        contact: state.contactStore.contact
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({getContact}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ContactsCallPage);
