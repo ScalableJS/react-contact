557065dc146c6867b7d3a5267e5b9e907b22ddb3
Oleg Ch <056@live.ru>
2019-05-20 14:50:24 +0300

    Render call history

--- src/components/user-call.js
+++ src/components/user-call.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import {ListGroup, Button} from 'react-bootstrap';
 
 export default function UserCall({contact}) {
+    const callTypes = ['Outgoing', 'Incoming', 'Didn\'t connect'];
+    // "timestamp": 1556176577183,
+    //     "duration": 1075937
+    // Load Contact Asynchronously
+    // <span>Duration {new Date(item.timestamp)}</span>
     return (
-        <div>Call history</div>
+        contact.callHistory.map((item) => {
+            const formatCallDate = new Intl.DateTimeFormat('en-GB', {
+                month: 'numeric',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric'
+            }).format(item.timestamp);
+
+            const formatCallDuration = new Intl.DateTimeFormat('en-GB', {
+                minute: 'numeric',
+                second: 'numeric'
+            }).format(item.duration);
+
+            return <ListGroup.Item key={item._id}>
+                <span>{formatCallDate}</span>
+
+                <span>{callTypes[item.type - 1]}</span>
+                <span>Duration: {formatCallDuration}</span>
+            </ListGroup.Item>
+        })
     )
 }
--- src/pages/contacts-call-page.js
+++ src/pages/contacts-call-page.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
 import {Redirect} from 'react-router';
-import UserCall from "../components/user-call";
-
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {Row, Col} from 'react-bootstrap';
-import {getContact, newContact, saveContact, updateContact} from "../actions";
-
+import {ListGroup} from 'react-bootstrap';
+import {getContact} from "../actions";
+import UserCall from "../components/user-call";
 
 class ContactsCallPage extends Component {
     state = {
@@ -22,15 +20,18 @@ class ContactsCallPage extends Component {
 
     render() {
         const {_id} = this.props.match.params;
+        const {contact} = this.props;
         if (this.state.redirect) {
             return <Redirect to="/"/>
         } else {
             if (_id) {
                 if (this.props.contact) {
                     return (
-                        <UserCall
-                            contact={this.props.contact}
-                        />)
+                        <ListGroup>
+                            <UserCall
+                                contact={contact}
+                            />
+                        </ListGroup>)
                 } else if (this.props.contact === null) {
                     return <h1>The contact is missing or deleted</h1>
                 } else {
