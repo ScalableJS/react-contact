f347925a5eda083e1852c6b1be1d3f50bf7f201f
Oleg Ch <056@live.ru>
2019-05-29 17:26:16 +0300

    add composeWithDevTools

--- src/store.js
+++ src/store.js
@@ -1,14 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from "redux-thunk";
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-import promise from "redux-promise-middleware";
 import allReducers from "./reducers";
-
-// const middleware = applyMiddleware(promise, thunk);
-// const store = (window.devToolsExtension
-//     ? window.devToolsExtension()(createStore)
-//     : createStore)(allReducers, middleware);
 
 let store = createStore(
     allReducers,
