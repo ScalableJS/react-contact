c64b5d59c30b05890272b806dced05648772da4a
Oleg Ch <056@live.ru>
2019-05-31 01:15:17 +0300

    combine contact reducer and index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,33 @@
 import {combineReducers} from 'redux';
-import ContactsReducer from './contacts-reducer';
-import { reducer as formReducer } from 'redux-form';
+import {reducer as formReducer} from 'redux-form';
+
+
+import contact from './contact';
+import contacts from './contacts';
+import deleteContact from './deleteContact';
+import saveContact from './saveContact';
+
+const defaultContact = {
+    name: {},
+    callHistory: []
+};
+const defaultState = {
+    contacts: [],
+    contact: defaultContact,
+    pending: false
+};
+
+const DEFAULT = (state) => state;
+
+const ContactsReducer = (state = defaultState, action) => {
+    return ({
+        ...contacts,
+        ...contact,
+        ...deleteContact,
+        ...saveContact
+    }[action.type] || DEFAULT)(state, action);
+};
+
 
 const allReducers = combineReducers({
     contactStore: ContactsReducer,
