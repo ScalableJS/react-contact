0bad55be7fbba1592d6ff48003fda5198ce246cd
Oleh_Cherkashchenko <oleg056@gmail.com>
2019-06-19 14:32:21 +0300

    avoid {{

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -7,32 +7,33 @@ import {mail2 as mail} from 'react-icons-kit/icomoon/mail2'
 import {arrowUpRight2} from 'react-icons-kit/icomoon/arrowUpRight2'
 import {arrowDownLeft2} from 'react-icons-kit/icomoon/arrowDownLeft2'
 
+const iconStyle = {width: 24, height: 24, display: 'inline-block', color: '#2A9D8F'};
 export const IconUser = () => (
-    <span className={'mr-1'} style={{width: 24, height: 24, display: 'inline-block', color: '#2A9D8F'}}>
+    <span className={'mr-1'} style={iconStyle}>
         <Icon size={'100%'} icon={user}/>
     </span>
 );
 
 export const IconPhone = () => (
-    <span className={'mr-1'} style={{width: 24, height: 24, display: 'inline-block', color: '#2A9D8F'}}>
+    <span className={'mr-1'} style={iconStyle}>
         <Icon size={'100%'} icon={phone}/>
     </span>
 );
 
 export const IconMail = () => (
-    <span className={'mr-1'} style={{width: 24, height: 24, display: 'inline-block', color: '#2A9D8F'}}>
+    <span className={'mr-1'} style={iconStyle}>
         <Icon size={'100%'} icon={mail}/>
     </span>
 );
 
 export const IconIncoming = () => (
-    <span className={'mr-1'} style={{width: 24, height: 24, display: 'inline-block', color: '#2A9D8F'}}>
+    <span className={'mr-1'} style={iconStyle}>
         <Icon size={'100%'} icon={arrowDownLeft2}/>
     </span>
 );
 
 export const IconOutgoing = () => (
-    <span className={'mr-1'} style={{width: 24, height: 24, display: 'inline-block', color: '#2A9D8F'}}>
+    <span className={'mr-1'} style={iconStyle}>
         <Icon size={'100%'} icon={arrowUpRight2}/>
     </span>
 );
